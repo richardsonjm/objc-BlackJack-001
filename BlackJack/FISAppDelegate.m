208603465371b305a9e9081f4cd360e4eaa64f8d
John Richardson <richardsonjm@gmail.com>
2016-05-18 10:23:47 -0400

    Build Player model

--- BlackJack/FISAppDelegate.m
+++ BlackJack/FISAppDelegate.m
@@ -1,11 +1,21 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISBlackjackPlayer.h"
+#import "FISCardDeck.h"
 
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+
+    FISBlackjackPlayer *john = [[FISBlackjackPlayer alloc] initWithName:@"john"];
+    FISCardDeck *cardDeck = [[FISCardDeck alloc] init];
+    FISCard *card = cardDeck.drawNextCard;
+    [john acceptCard:card];
+
+    NSLog(@"%@", john);
+
     return YES;
 }
 
--- BlackJack/FISBlackjackPlayer.m
+++ BlackJack/FISBlackjackPlayer.m
@@ -32,15 +32,55 @@
 }
 
 - (void)resetForNewGame {
-    
+    [self.cardsInHand removeAllObjects];
+    self.handscore = 0;
+    self.aceInHand = NO;
+    self.stayed = NO;
+    self.blackjack = NO;
+    self.busted = NO;
 }
 
 - (void)acceptCard:(FISCard *)card {
-    
+    [self.cardsInHand addObject:card];
+    self.handscore = self.handscore + card.cardValue;
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"rank CONTAINS %@", @"A"];
+    NSArray *aces = [self.cardsInHand filteredArrayUsingPredicate:predicate];
+    if ([aces count] > 0) {
+        self.aceInHand = YES;
+    }
+    if ([aces count] == 1 && self.handscore < 12) {
+        self.handscore = self.handscore + 10;
+    }
+    if (self.handscore > 21) {
+        self.busted = YES;
+    }
+    if (self.handscore == 21 && [self.cardsInHand count] == 2) {
+        self.blackjack = YES;
+    }
 }
 
 - (BOOL)shouldHit {
-    return NO;
+    if (self.handscore < 17) {
+        return YES;
+    } else {
+        self.stayed = YES;
+        return NO;
+    }
+}
+
+- (NSString *)description {
+    NSMutableString *description = [NSMutableString stringWithFormat:@" name: %@\n cards:", self.name];
+    for (FISCard *card in self.cardsInHand) {
+        [description appendString: card.cardLabel];
+    }
+    [description appendString:[NSString stringWithFormat:@"\n handscore: %lu\n", self.handscore]];
+    [description appendString:[NSString stringWithFormat:@" ace in hand: %d\n", self.aceInHand]];
+    [description appendString:[NSString stringWithFormat:@" stayed: %d\n", self.stayed]];
+    [description appendString:[NSString stringWithFormat:@" blackjack: %d\n", self.blackjack]];
+    [description appendString:[NSString stringWithFormat:@" busted: %d\n", self.busted]];
+    [description appendString:[NSString stringWithFormat:@" wins: %lu\n", self.wins]];
+    [description appendString:[NSString stringWithFormat:@" losses: %lu\n", self.losses]];
+    return description;
 }
 
 @end
